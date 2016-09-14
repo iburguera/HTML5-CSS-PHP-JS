10761f34e5e8dcd94cf2bbb62a520d49676c5aa6
Iker Burguera <ikerburguera@gmail.com>
2016-09-14 19:48:51 +0200

    Update index.php

--- MagicMirror/Version_0/index.php
+++ MagicMirror/Version_0/index.php
@@ -3,10 +3,9 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>MagicMirror v0 - Iker Burguera</title>
-
 <link rel="stylesheet" type="text/css" href="css/cssTabla.css" />
 
-<meta http-equiv="refresh" content="60">
+<meta http-equiv="refresh" content="60">  <!-- Refresh every 60" -->
 
 <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js"></script>
@@ -18,39 +17,38 @@
 
 <body>
 
-
 <div id="weather">
 
 	<div id="hora"></div>
 	<div id="dia"></div>
 
 	<div id="tiempo">
-    	<div id="iconoActual"></div>
-        <div id="tempActual"></div>
-        <div id="tempMax"></div>
-        <div id="tempMin"></div>
-        <div id="ciudadActual"></div>
-        
-	<div id="forecastIcon1"></div>
-        <div id="forecastDay1"></div>
-        <div id="forecastTemp1"></div>
-        
-        <div id="forecastIcon2"></div>
-        <div id="forecastDay2"></div>
-        <div id="forecastTemp2"></div> 
-        
-        <div id="forecastIcon3"></div>
-        <div id="forecastDay3"></div>
-        <div id="forecastTemp3"></div> 
-        
-        <div id="forecastIcon4"></div>
-        <div id="forecastDay4"></div>
-        <div id="forecastTemp4"></div>   
-        
-        <div id="forecastIcon5"></div>
-        <div id="forecastDay5"></div>
-        <div id="forecastTemp5"></div>         
-    </div>  <!--end of Tiempo-->
+	    	<div id="iconoActual"></div>
+	        <div id="tempActual"></div>
+	        <div id="tempMax"></div>
+	        <div id="tempMin"></div>
+	        <div id="ciudadActual"></div>
+	        
+		<div id="forecastIcon1"></div>
+	        <div id="forecastDay1"></div>
+	        <div id="forecastTemp1"></div>
+	        
+	        <div id="forecastIcon2"></div>
+	        <div id="forecastDay2"></div>
+	        <div id="forecastTemp2"></div> 
+	        
+	        <div id="forecastIcon3"></div>
+	        <div id="forecastDay3"></div>
+	        <div id="forecastTemp3"></div> 
+	        
+	        <div id="forecastIcon4"></div>
+	        <div id="forecastDay4"></div>
+	        <div id="forecastTemp4"></div>   
+	        
+	        <div id="forecastIcon5"></div>
+	        <div id="forecastDay5"></div>
+	        <div id="forecastTemp5"></div>         
+    	</div>  <!--end of Tiempo-->
 
 	<div id="frase"></div>
 
